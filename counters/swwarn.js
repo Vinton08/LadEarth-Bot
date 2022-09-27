5af76de90e00350f1264552d586bd83d977ffa19
VintonNotFound <hester.vinton@outlook.com>
2022-09-27 19:32:54 -0400

    Fixed bugs with Severe warn command

--- counters/swwarn.js
+++ counters/swwarn.js
@@ -1,8 +1,8 @@
 module.exports = async (client) =>{
     const guild = client.guilds.cache.get('738155860739620994');
     setInterval(() =>{
-        const channel = guild.channels.cache.get('963605173685587979')
-        messsage.channel.send('**These Tweets can sometimes be minutes behind. Do not use this for life saving notifications!**');
+        const channel = guild.channels.cache.get('963605173685587979');
+        channel.send('**These Tweets can sometimes be minutes behind. Do not use this for life saving notifications!**');
         console.log('Severe Warning Issued.');
     },43200000);
 }
