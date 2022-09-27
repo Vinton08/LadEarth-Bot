20344c820173a953148b8da7bf6b8123dded7e9e
VintonNotFound <82289738+VintonNotFound@users.noreply.github.com>
2022-09-27 12:42:16 -0400

    Shortened time between member counter

--- counters/member-counter.js
+++ counters/member-counter.js
@@ -5,5 +5,5 @@ module.exports = async (client) =>{
         const channel = guild.channels.cache.get('946513912554192976')
         channel.setName(`Total Members: ${memberCount.toLocaleString()}`);
         console.log('Updating Member Count')
-    },600000);
+    },300000);
 }
