97fd3e667c862ba4353c7e88efcb3a0abbd4b771
VintonNotFound <82289738+VintonNotFound@users.noreply.github.com>
2022-10-12 08:50:45 -0400

    adminc5 update

--- commands/adminc5.js
+++ commands/adminc5.js
@@ -1,3 +1,18 @@
+<<<<<<< HEAD
+module.exports = {
+    name:'adminc5',
+    description:'gpac',
+    execute(message, args, cmd, client, discord){
+        if(message.member.roles.cache.has('760189351735984211')){
+            const channel = guild.channels.cache.get('963605173685587979');
+            channel.send('**These Tweets can sometimes be minutes behind. Do not use this for life saving notifications!**');
+            console.log('Severe warning override used!')
+        }else{
+            message.channel.send(`You do not have permission to run this command!`);
+        }
+    }
+}
+=======
 module.exports = {
     name: 'adminc5',
     description: 'gpac',
@@ -14,3 +29,4 @@ module.exports = {
 }
     }
 }
+>>>>>>> f438fcfb430437937b13b7ba762c49416524db7c
